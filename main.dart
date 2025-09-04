a27272f2524a1975b8731ae1373e9d5bf4f42ef4
Leehs21 <leticiasoares2102@gmail.com>
2025-09-04 00:01:41 +0000

    feat: if statement

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main(){
+    int idade = 5;
+    if (idade >= 18)
+    {
+        print('Pode entrar na balada');        
+    }
+    else{
+        print('Nao pode entrar');
+    }
+    print ('fim do programa');
+    
+}
